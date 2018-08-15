65586cd8c2c92af08e204ecc6c58ce6564ed6bc9
Bryant Fukushima <bmfukushima94@gmail.com>
2018-08-15 03:31:43 -0700

    bamazon customer

---
+++ bamazonCustomer.js
@@ -0,0 +1,66 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var cTable = require("console.table")
+
+var connection = mysql.createConnection({
+	host: "localhost",
+	port: 3306,
+	user: "root",
+	password: "",
+	database: "bamazon_db"
+});
+
+connection.connect(function(err) {
+	if(err) {
+		throw err;
+	} else {
+		runApp();
+	}
+});
+
+function runApp() {
+	var query = connection.query("SELECT * FROM products" , function(err , show_items) {
+
+		//display as table
+		console.table(show_items);
+
+		inquirer.prompt([
+			{
+				type: "input",
+				name: "buyID",
+				message: "Select item ID of the product you would like to purchase.",
+			},
+			{
+				type: "input",
+				name: "amount",
+				message: "Quantity:"
+			}
+
+		]).then(function(data) {
+
+						var stockNum = show_items[data.buyID - 1].stock_quantity;
+						var newStock = stockNum - data.amount;
+						if (stockNum < data.amount) {
+							console.log("Insufficient Stock");
+							runApp();
+						} else {
+							var query = connection.query("UPDATE products SET ? WHERE ? ", 
+								[
+									{
+										stock_quantity: newStock
+									},
+									{
+										item_id: data.buyID
+									}
+								], 
+								function(err , res) {
+									var total = show_items[data.buyID - 1].price * data.amount;
+										console.log("Total Amount Spent: $" + total);
+										//end connection
+										connection.end();
+								});
+						}
+
+		});
+	});
+}
---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO products (product_name , department_name , price , stock_quantity)
+VALUES ("Blue Jacket" , "Outerwear" , 120 , 50),
+("No Dye Jacket" , "Outerwear" , 200 , 75),
+("Footprint Shoes" , "Footwear" , 89.95 , 10),
+("Grass Slippers" , "Footwear" , 450.50 , 3),
+("Convertible Snowpants to Swim Trunks" , "Bottoms" , 115.95 , 40),
+("Khaki Jeans" , "Bottoms" , 75 , 97),
+("Anywhere Surfboard" , "Gear" , 325.50 , 10),
+("Tent" , "Gear" , 90.99 , 50),
+("12pc Wooden Straws" , "Accessories" , 10 , 200),
+("Pollution Detecting Sunglasses" , "Accessories" , 700.35 , 5);
